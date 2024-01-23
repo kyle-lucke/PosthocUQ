a2510a083f2ce4667804f387f9191e52bd5ab2e3
kyle-lucke <luck0692@vandals.uidaho.edu>
2024-01-23 10:48:35 -0700

    Move custom layers to classes, forgot to import vgg model for imagenet init.

--- models/small_convnet_svhn.py
+++ models/small_convnet_svhn.py
@@ -2,7 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from custom_layers import *
+# Original implementation from ConfidNet:
+# https://github.com/valeoai/ConfidNet/blob/master/confidnet/models/small_convnet_svhn.py 
+class Conv2dSame(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size, bias=True, padding_layer=nn.ReflectionPad2d
+    ):
+        super().__init__()
+        ka = kernel_size // 2
+        kb = ka - 1 if kernel_size % 2 == 0 else ka
+        self.net = nn.Sequential(
+            padding_layer((ka, kb, ka, kb)),
+            nn.Conv2d(in_channels, out_channels, kernel_size, bias=bias),
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 class SmallConvNetSVHN_BaseModel(nn.Module):
   
